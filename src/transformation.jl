94d6c801b0d1f20ed8c86bea5373cc9af9ea70ae
frankill <34159384+frankill@users.noreply.github.com>
2018-08-28 22:26:51 +0800

    Update transformation.jl

--- src/transformation.jl
+++ src/transformation.jl
@@ -3,22 +3,51 @@ struct SearchNode{T} end
 
 const  sname = ["filter", "must", "must_not"]
 
-function make_json(::Type{MakeType{:filter}}, exprs::Vector )
+function make_json(method::AbstractString, exprs::Vector )
 	quote
 		len = length($exprs)
 		val = Array{Dict}(undef, length($exprs))
+		
 		for i in 1:len
 			(methods, name, content)  = trans($exprs[i])
 			val[i] = Dict(methods => Dict(name => eval(content ))) 
 		end 
-		Dict("filter" => val)
+		
+		Dict($method => val)
+	end 	
+end
+
+function make_json( exprs::Vector )
+	quote
+		len = length($exprs)
+		val = Array{Dict}(undef, length($exprs))
+		
+		for i in 1:len
+			(methods, name, content)  = trans($exprs[i])
+			val[i] = Dict(methods => Dict(name => eval(content ))) 
+		end 
+		
+		Dict("query" => Dict("bool" => val))
+	 
 	end 	
 end
  
-
 macro filter( expr... )
-    return make_json( MakeType{:filter}, collect( expr ))
+    return make_json( "filter", collect( expr ))
 end
+
+macro must( expr... )
+    return make_json( "must", collect( expr ))
+end
+
+macro must_not( expr... )
+    return make_json( "must_not", collect( expr ))
+end
+
+macro query( expr... )
+    return make_json( collect( expr ))
+end
+
 
 function trans( expr::Expr )
     trans( SearchNode{expr.head}, expr)
@@ -41,7 +70,10 @@ function trans(::Type{SearchNode{:(has)}}, expr::Expr)
 end 
 
 function trans(::Type{SearchNode{:(>)}}, expr::Expr)
-	("range" , expr.args[2] , expr.args[3] )
+	("range" , expr.args[2] , Expr(:call, :Dict , "gte" => expr.args[3]) )
 end 
 
+function trans(::Type{SearchNode{:(<)}}, expr::Expr)
+	("range" , expr.args[2] , Expr(:call, :Dict , "lte" => expr.args[3]) )
+end 
  
