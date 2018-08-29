da8cb6240845cb2ff74f0c54d01e3afbf20311af
frankill <34159384+frankill@users.noreply.github.com>
2018-08-29 21:50:33 +0800

    Update transformation.jl

--- src/transformation.jl
+++ src/transformation.jl
@@ -1,61 +1,91 @@
-struct MakeType{T} end  
 struct SearchNode{T} end
 
-const  sname = ["filter", "must", "must_not"]
+const  sname = ["filter", "must", "must_not", "query" , "nested"]
+
+function Base.push!(t::AbstractDict, b::AbstractDict)
+	for (x,y) in b 
+		push!(t, x => y)
+	end 
+end  
 
 function make_json(method::AbstractString, exprs::Vector )
-	quote
-		len = length($exprs)
-		val = Array{Dict}(undef, length($exprs))
-		
-		for i in 1:len
-			(methods, name, content)  = trans($exprs[i])
-			val[i] = Dict(methods => Dict(name => eval(content ))) 
-		end 
-		
-		Dict($method => val)
-	end 	
+	esc(quote
+			local len = length($exprs)
+			local val = Array{Dict}(undef, length($exprs))
+			
+			for i in 1:len
+				(methods, name, content)  = trans($exprs[i])
+				if string(name) in sname  
+					val[i] =  eval(content) 
+				else 
+					val[i] = Dict(methods => Dict(name =>  eval(content)   )) 
+				end 
+			end 
+			
+			Dict($method => val)
+	end)
+end
+
+function make_json(  exprs::Vector, type::AbstractString )
+	esc(quote
+			local len = length($exprs)
+			local val = Dict()
+			
+			for i in 1:len
+				(methods, name, content)  = trans($exprs[i])
+				if string(name) in sname  
+					push!(val, eval(content ))
+				else 
+					push!(val, name => eval(content )) 
+				end 
+			end 
+			
+			Dict($type => val)
+	end)	
 end
 
 function make_json( exprs::Vector )
-	quote
-		len = length($exprs)
-		val = Dict()
-		query = Dict()
-		
-		for i in 1:len
-			(methods, name, content)  = trans($exprs[i])
+	esc(quote
+			local len = length($exprs)
+			local val = Dict()
+			local query = Dict()
 			
-			if string(name) in sname  
-				temp = eval(content )
-				push!(query, string(name) => temp[string(name)] )
-			else 
-				push!(val, name => eval(content )) 
+			for i in 1:len
+				(methods, name, content)  = trans($exprs[i])
+				
+				if string(name) in sname  
+					push!(query, eval(content ))
+				else 
+					push!(val, name => eval(content )) 
+				end 
 			end 
-		end 
-		
-		if ! isempty(query)
-			merge(val, Dict("query" => Dict( "bool" => query)))
-		else 
-			val
-		end 
-	end 
+			
+			if ! isempty(query)
+				merge(val,  Dict("query" => Dict( "bool" => query)))
+			else 
+				val
+			end 
+	end)
 end
  
 macro filter( expr... )
-    return make_json( "filter", collect( expr ))
+    return  make_json( "filter", collect( expr ))
 end
 
 macro must( expr... )
-    return make_json( "must", collect( expr ))
+    return make_json( "must", collect( expr )) 
 end
 
 macro must_not( expr... )
-    return make_json( "must_not", collect( expr ))
+    return  make_json( "must_not", collect( expr )) 
 end
 
 macro query( expr... )
-    return make_json( collect( expr ))
+    return  make_json( collect( expr )) 
+end
+
+macro nested( expr... )
+    return  make_json(collect( expr ) ,"nested") 
 end
 
 
@@ -68,7 +98,7 @@ function trans( ::Type{SearchNode{:call}}, expr::Expr)
 end
 
 function trans(::Type{SearchNode{:(=)}}, expr::Expr)
-	("term" , expr.args[1] , expr.args[2] )
+	("term" , expr.args[1] ,  expr.args[2] )
 end 
 
 function trans(::Type{SearchNode{:(in)}}, expr::Expr)
@@ -80,10 +110,9 @@ function trans(::Type{SearchNode{:(has)}}, expr::Expr)
 end 
 
 function trans(::Type{SearchNode{:(>)}}, expr::Expr)
-	("range" , expr.args[2] , Expr(:call, :Dict , "gte" => expr.args[3]) )
+	("range" , expr.args[2] , :(Dict( "gte" => $(expr.args[3])) ))
 end 
 
 function trans(::Type{SearchNode{:(<)}}, expr::Expr)
-	("range" , expr.args[2] , Expr(:call, :Dict , "lte" => expr.args[3]) )
+	("range" , expr.args[2] , :(Dict( "lte" => $(expr.args[3])) ))
 end 
- 
