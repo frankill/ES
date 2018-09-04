8b652605bfb761c5c39e08e5e17f8718e7c326d5
frankill <34159384+frankill@users.noreply.github.com>
2018-09-04 17:39:42 +0800

    Update ES.jl

--- src/ES.jl
+++ src/ES.jl
@@ -2,10 +2,12 @@
 
 	using HTTP
 	using JSON
-	export Esinfo, escount, esearch ,escroll, @esexp ,@esexport  ,@query, @filter, @must, @must_not 
+	export Esinfo, escount, esearch ,escroll, trans ,sname ,esbulkupdate,esbulkcript,esbulkindex,esbulkcreate
+	export @esexp ,@esexport ,@query, @filter, @must, @must_not  ,@nested ,@has_child, @has_parent ,@fulltext
+
 
 	include("api.jl")
 	include("Transformation.jl")
-	#include("baidumap.jl")
+	include("baidumap.jl")
 
 end 
