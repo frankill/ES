b8fe50ce96dbeb365e03c11b696a5c2607b99638
frankill <34159384+frankill@users.noreply.github.com>
2018-09-05 11:28:28 +0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,6 @@
 @fulltext(match = @query(type = "test"), bool = @filter(has("openid")))
 @query(size = 1000, @filter(has("openid")))
 @query(size = 1000, query = @filter(has("openid")))
+
+a = :( 1< a <=4)
+@query(a)
