48351c7dadc8b1d7ad18fa6737184af85ff692ff
frankill <34159384+frankill@users.noreply.github.com>
2018-09-05 10:57:55 +0800

    Update transformation.jl

--- src/transformation.jl
+++ src/transformation.jl
@@ -144,9 +144,9 @@ end
 abstract type esyms{T} end 
 
 syms(::Type{esyms{:<}})    = "gt"
-syms(::Type{esyms{:<=}})   = "gte"
+syms(::Type{esyms{:(<=)}})   = "gte"
 syms(::Type{esyms{:>}})    = "lt"
-syms(::Type{esyms{:>=}})   = "lte" 
+syms(::Type{esyms{:(>=)}})   = "lte" 
 
 Base.:!(::Type{esyms{:<}})  = esyms{:>}
 Base.:!(::Type{esyms{:(<=)}}) = esyms{:(>=)}
