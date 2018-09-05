cb7f1cf069bd6067362c40d153b637d8aa4000b7
frankill <34159384+frankill@users.noreply.github.com>
2018-09-05 10:13:06 +0800

    Update ES.jl

--- src/ES.jl
+++ src/ES.jl
@@ -2,8 +2,8 @@ __precompile__()
 
 module ES
 
-	using HTTP
-	using JSON
+	import HTTP
+	import JSON
 	export Esinfo, escount, esearch ,escroll, trans ,sname ,esbulkupdate,esbulkcript,esbulkindex,esbulkcreate
 	export @esexp ,@esexport ,@query, @filter, @must, @must_not  ,@nested ,@has_child, @has_parent ,@fulltext
 
