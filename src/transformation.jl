fa7c936c028ed452689f19340f417d82987cef8a
frankill <34159384+frankill@users.noreply.github.com>
2018-09-10 09:55:08 +0800

    Update transformation.jl

--- src/transformation.jl
+++ src/transformation.jl
@@ -175,7 +175,7 @@ function make_json(method::AbstractString, exprs::Vector )
 			val[i] =  :( $methods => Dict( $name  => eval($content)) ) 
 		end 
 	end 
-        esc(:(Dict($method => map(eval, $val) )))
+ 	Expr( :call, :vcat, val... ) |> df -> esc(:(Dict($method => $df )))
 end
 
 function make_json(  exprs::Vector, type::AbstractString )
