ed84bc0375074c243e1155b4adc6f92ded11b4f7
frankill <34159384+frankill@users.noreply.github.com>
2018-09-13 13:00:58 +0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,25 +1,26 @@
-@fulltext(match = @query(type = "test")) 
-@fulltext(match = @query(type = "test"), bool = @filter(has("openid")))
-@query(size = 1000, @filter(has("openid")))
-@query(size = 1000, query = @filter(has("openid")))
-
-
-ES.esfsearch(info, index, @query(size=100) )
-            
-          
-            
 using Distributed 
 addprocs(5)
 
 @everywhere using ES 
-@everywhere using SharedArrays
 
 info     = Esinfo("", "") 
 index = "201807"
 setting = ES.esindexsetting(info, index)[index]["settings"]["index"]
 shards = parse(Int, setting["number_of_shards"]) 
- 
+
+# 1
 @distributed (vcat) for i in 0:shards-1  
 	query= @common(slice= @query(id=i, max= shards) ) |>  df ->  merge(df , @query(size = 100) )  
 	ES.esfsearch(info, index, query)  
 end 
+
+
+#2
+@fulltext(match = @query(type = "test")) 
+@fulltext(match = @query(type = "test"), bool = @filter(has("openid")))
+@query(size = 1000, @filter(has("openid")))
+@query(size = 1000, query = @filter(has("openid")))
+
+						
+#3
+ES.esfsearch(info, index, @query(size=100) )
