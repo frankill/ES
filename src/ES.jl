98403a391929d454e0f21c402fd107d04e25aeeb
frankill <34159384+frankill@users.noreply.github.com>
2018-09-17 16:58:28 +0800

    Update ES.jl

--- src/ES.jl
+++ src/ES.jl
@@ -4,7 +4,7 @@ module ES
 
 	using HTTP
 	using JSON
-	export Esinfo, escount, esearch ,escroll, estrans ,sname ,esbulkupdate,esbulkcript,esbulkindex,esbulkcreate
+	export Esinfo, escount, esearch ,escroll ,esbulkupdate,esbulkcript,esbulkindex,esbulkcreate
 	export @esexp ,@esexport ,@query, @filter, @must, @must_not  ,@nested ,@has_child, @has_parent ,@fulltext,@common
 
 
