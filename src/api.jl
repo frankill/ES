0af1ef3881945685a723acaa21b7ad744e6e7240
frankill <34159384+frankill@users.noreply.github.com>
2018-09-17 12:28:49 +0800

    Update api.jl

--- src/api.jl
+++ src/api.jl
@@ -150,7 +150,7 @@ function esfsearch(info::Esinfo, index::AbstractString, body::T ; kw... ) where 
 	typeof(body) <: AbstractString && (body= JSON.Parser.parse(body))
 
 	num   = pop!(body, "size", 10000)
-	snum  = escount(info, index, @common(query= querys["query"]) )
+	snum  = escount(info, index, @common(query= body["query"]) )
 	query = Dict(kw..., :size => num)
 			
 	res   = esearch(info, index, body, query)  
