e93b2f032308dc5e3f46b250c1a2c9743d7f18dd
frankill <34159384+frankill@users.noreply.github.com>
2018-09-19 11:04:25 +0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,18 @@
-using Distributed 
-addprocs(5)
+# using Distributed 
+# addprocs(5)
 
-@everywhere using ES 
+# @everywhere using ES 
 
-info     = Esinfo("", "") 
-index = "201807"
-setting = ES.esindexsetting(info, index)[index]["settings"]["index"]
-shards = parse(Int, setting["number_of_shards"]) 
+# info     = Esinfo("", "") 
+# index = "201807"
+# setting = ES.esindexsetting(info, index)[index]["settings"]["index"]
+# shards = parse(Int, setting["number_of_shards"]) 
 
-# 1
-@distributed (vcat) for i in 0:shards-1  
-	query= @common(slice= @query(id=i, max= shards) ) |>  df ->  merge(df , @query(size = 100) )  
-	ES.esfsearch(info, index, query)  
-end 
+# # 1
+# @distributed (vcat) for i in 0:shards-1  
+# 	query= @common(slice= @query(id=i, max= shards) ) |>  df ->  merge(df , @query(size = 100) )  
+# 	ES.esfsearch(info, index, query)  
+# end 
 
 
 #2
