0f96391215e020232b31bce1336da5f7319f3524
frankill <34159384+frankill@users.noreply.github.com>
2018-11-20 14:41:00 +0800

    Update plugins.jl

--- src/plugins.jl
+++ src/plugins.jl
@@ -9,30 +9,17 @@ function makeurl(::Type{Xpack{:translate}}, info::Esinfo)
 	"http://$(info.host):$(info.port)/_xpack/sql/translate"
 end
 
-function essql(info::Esinfo, sql::AbstractString ; kw...)
+macro xpackfun(interface, d) 
+	funcname = esc(Symbol(interface)) 
+	return quote 
+		function $(funcname)(info::Esinfo, sql::$d ; kw...)  
+			query = Dict(kw...) 
+			@esexport "POST" makeurl(Xpack{:translate}, info) json(sql) query "application/json"
+		end 
+	end 
+end 
 
-	query = Dict(kw...) 
-	@esexport "POST" makeurl(Xpack{:sql}, info) sql query "application/json"
-
-end
-
-function essql(info::Esinfo, sql::Dict ; kw...)
-
-	query = Dict(kw...) 
-	@esexport "POST" makeurl(Xpack{:sql}, info) json(sql) query "application/json"
-
-end
-
-function essql(info::Esinfo, sql::AbstractString ; kw...)
-
-	query = Dict(kw...) 
-	@esexport "POST" makeurl(Xpack{:translate}, info) sql query "application/json"
-
-end
-
-function essql(info::Esinfo, sql::Dict ; kw...)
-
-	query = Dict(kw...) 
-	@esexport "POST" makeurl(Xpack{:translate}, info) json(sql) query "application/json"
-
-end
+@xpackfun "essqltrans" Dict
+@xpackfun "essqltrans" AbstractString
+@xpackfun "essql" Dict
+@xpackfun "essql" AbstractString
