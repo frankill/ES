f2343b45d9de0705abe3ac5f00294878ed06a698
frankill <34159384+frankill@users.noreply.github.com>
2018-11-20 15:34:46 +0800

    Update plugins.jl

--- src/plugins.jl
+++ src/plugins.jl
@@ -20,7 +20,7 @@ macro xpackfun(interface)
 	return quote 
 		function ($funname)(info::Esinfo, sql::T; kw...) where T <: Union{AbstractString,Dict}
 			query = Dict(kw...) 
-			isa(sql, Dict) && sql = json(sql) 
+			isa(sql, Dict) && (sql = json(sql)) 
 			@esexport "POST" makeurl(Xpack{$(iname)}, info) sql query "application/json"
 		end 
 	end 
