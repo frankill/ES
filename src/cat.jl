8330fe8f2444cec17a611681d02b842c6df8094c
frankill <34159384+frankill@users.noreply.github.com>
2018-11-26 14:19:21 +0800

    Update cat.jl

--- src/cat.jl
+++ src/cat.jl
@@ -1,73 +1,67 @@
+
 struct CatType{T} end
 
-function makeurl(::Type{CatType{:_cat}}, info::Esinfo, name::AbstractString)
-	"http://$(info.host):$(info.port)/_cat/aliases/$name"
+function makeurl(::Type{CatType{:_cat}}, info::Esinfo, paths::AbstractString, name::AbstractString)
+	"http://$(info.host):$(info.port)/_cat/$paths/$name"
 end
 
-function makeurl(::Type{CatType{:_cat}}, info::Esinfo)
-	"http://$(info.host):$(info.port)/_cat/aliases"
+function makeurl(::Type{CatType{:_cat}}, info::Esinfo,paths::AbstractString)
+	"http://$(info.host):$(info.port)/_cat/$paths"
 end
 
-function makeurl(::Type{CatType{:_allocation}}, info::Esinfo, node_id::AbstractString)
-	"http://$(info.host):$(info.port)/_cat/allocation/$name"
+macro catexport(method, url, body , query , type )
+
+	header = ["content-type" => "$type" ]
+	esc(
+		quote
+
+			respos = HTTP.request($method, $url , $header , $body, query= $query)
+			String(respos.body) |> println
+
+		end )
 end
 
-function makeurl(::Type{CatType{:_allocation}}, info::Esinfo)
-	"http://$(info.host):$(info.port)/_cat/allocation"
-end
+macro catgenfun(methods, paths, names )
+	funname = esc(Symbol(string("cat", paths )) )
 
-function makeurl(::Type{CatType{:_count}}, info::Esinfo, node_id::AbstractString)
-	"http://$(info.host):$(info.port)/_cat/count/$name"
-end
+	return quote 
+		( if $names != nothing 
 
-function makeurl(::Type{CatType{:_count}}, info::Esinfo)
-	"http://$(info.host):$(info.port)/_cat/count"
-end
+			iname = Symbol($names)
+			function $(funname)(info::Esinfo, iname::AbstractString ; kw...)  
+				querys = Dict(kw...) 
+				url    = makeurl(CatType{:_cat}, info, $paths, iname)
+				@catexport "GET" url Dict() querys "application/json"
+			end 
+		else 
+			nothing 
+		end  ,
+		function $(funname)(info::Esinfo ;kw...)  
+			querys = Dict(kw...) 
+			url    = makeurl(CatType{:_cat}, info, $paths)
+			@catexport "GET" url Dict() querys "application/json"
+		end 
+		)
+	end
+end 
 
-function esaliases(info::Esinfo, name::AbstractString ;kw...)
 
-	query = Dict(kw...)
-	url   = makeurl(CatType{:_cat}, info, name )
-	@esexport "GET" url json(body) query "application/json"
+@catgenfun "GET" "aliases"  "name"
+@catgenfun "GET" "fielddata" "fields"
+@catgenfun "GET" "allocation"  "node_id"
+@catgenfun "GET" "count"  "index"
+@catgenfun "GET" "health"  nothing
+@catgenfun "GET" "master"  nothing 
+@catgenfun "GET" "nodeattrs"  nothing
+@catgenfun "GET" "pending_tasks"  nothing 
+@catgenfun "GET" "plugins"  nothing 
+@catgenfun "GET" "recovery"  "index" 
+@catgenfun "GET" "repositories"  nothing 
+@catgenfun "GET" "segments"  "index" 
+@catgenfun "GET" "shards"  "index" 
+@catgenfun "GET" "snapshots"  "repository" 
+@catgenfun "GET" "tasks"  nothing 
+@catgenfun "GET" "templates"  "name" 
+@catgenfun "GET" "thread_pool"  "thread_pool_patterns" 
 
-end
 
-function esaliases(info::Esinfo ;kw...)
-
-	query = Dict(kw...)
-	url   = makeurl(CatType{:_cat}, info )
-	@esexport "GET" url json(body) query "application/json"
-
-end
-
-function esallocation(info::Esinfo, node_id::AbstractString ;kw...)
-
-	query = Dict(kw...)
-	url   = makeurl(CatType{:_allocation}, info, node_id )
-	@esexport "GET" url json(body) query "application/json"
-
-end
-
-function esallocation(info::Esinfo ;kw...)
-
-	query = Dict(kw...)
-	url   = makeurl(CatType{:_allocation}, info )
-	@esexport "GET" url json(body) query "application/json"
-
-end
-
-function escatcount(info::Esinfo, index::AbstractString ;kw...)
-
-	query = Dict(kw...)
-	url   = makeurl(CatType{:_count}, info, node_id )
-	@esexport "GET" url json(body) query "application/json"
-
-end
-
-function escatcount(info::Esinfo ;kw...)
-
-	query = Dict(kw...)
-	url   = makeurl(CatType{:_count}, info )
-	@esexport "GET" url json(body) query "application/json"
-
-end
