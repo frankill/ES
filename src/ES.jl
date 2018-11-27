f99745e87e1aeb588ffef0fbfface332162c7c0a
frankill <34159384+frankill@users.noreply.github.com>
2018-11-27 14:48:15 +0800

    Update ES.jl

--- src/ES.jl
+++ src/ES.jl
@@ -8,10 +8,7 @@ module ES
 	export xpacksql , xpacktranslate
 	export @esexport ,@query, @filter, @must, @must_not ,@should  ,@nested ,@has_child, @has_parent ,@fulltext,@smi,@comm,@extra
 
-	export esbulkdel,escape,escreate,escrollclear,esdelete,esdelete_by_query,esdelete_by_query_rethrottle,esexists,
-	esexists_source,esexplain,esfield_caps,esget,esgetscript,esgetsource,esindexsetting,esmget,esmtermvectors,esping,
-	esputscript,esrankeval,esreindex,esreindex_rethrottle,esrender_search_template,esscripts_painless_execute,
-	essearch_shards,estrans,esupdate,esupdate_by_query,esupdate_by_query_rethrottle
+	export esbulkdel,escape,escreate,escrollclear,esdelete,esdelete_by_query,esdelete_by_query_rethrottle,esexists,esexists_source,esexplain,esfield_caps,esget,esgetscript,esgetsource,esindexsetting,esmget,esmtermvectors,esping,esputscript,esrankeval,esreindex,esreindex_rethrottle,esrender_search_template,esscripts_painless_execute,essearch_shards,estrans,esupdate,esupdate_by_query,esupdate_by_query_rethrottle
 	
 	include("transformation.jl")
 	include("api.jl")
