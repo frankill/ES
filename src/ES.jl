c3703b9b3d2cfa23f8cd8be9177186cc5e590a91
frankill <34159384+frankill@users.noreply.github.com>
2018-11-27 15:21:17 +0800

    Update ES.jl

--- src/ES.jl
+++ src/ES.jl
@@ -8,7 +8,13 @@ module ES
 	export xpacksql , xpacktranslate
 	export @esexport ,@query, @filter, @must, @must_not ,@should  ,@nested ,@has_child, @has_parent ,@fulltext,@smi,@comm,@extra
 
-	export esbulkdel,escape,escreate,escrollclear,esdelete,esdelete_by_query,esdelete_by_query_rethrottle,esexists,esexists_source,esexplain,esfield_caps,esget,esgetscript,esgetsource,esindexsetting,esmget,esmtermvectors,esping,esputscript,esrankeval,esreindex,esreindex_rethrottle,esrender_search_template,esscripts_painless_execute,essearch_shards,estrans,esupdate,esupdate_by_query,esupdate_by_query_rethrottle
+	export esbulkdel,escreate,escrollclear,esdelete,esdelete_by_query,esdelete_by_query_rethrottle,
+		esexists,esexists_source,esexplain,esfield_caps,esget,esgetscript,esgetsource,esindexsetting,
+		esmget,esmtermvectors,esping,esputscript,esrankeval,esreindex,esreindex_rethrottle,
+		esrender_search_template,esscripts_painless_execute,essearch_shards,esupdate,
+		esupdate_by_query,esupdate_by_query_rethrottle,cataliases,catfielddata,
+		catnodeattrs,catrecovery,catshards,cattemplates,catallocation,cathealth,catpending_tasks,
+		catrepositories,catsnapshots,catthread_pool,catcount,catmaster,catplugins,catsegments,cattasks
 	
 	include("transformation.jl")
 	include("api.jl")
