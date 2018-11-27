715ada1a747792d270ac0dbcb5a73725b66b5897
frankill <34159384+frankill@users.noreply.github.com>
2018-11-27 14:43:05 +0800

    Update ES.jl
    增加新的接口函数

--- src/ES.jl
+++ src/ES.jl
@@ -7,6 +7,11 @@ module ES
 	export Esinfo, escount, esearch ,esfsearch, escroll ,esbulkupdate,esbulkcript,esbulkindex,esbulkcreate
 	export xpacksql , xpacktranslate
 	export @esexport ,@query, @filter, @must, @must_not ,@should  ,@nested ,@has_child, @has_parent ,@fulltext,@smi,@comm,@extra
+
+	export esbulkdel,escape,escreate,escrollclear,esdelete,esdelete_by_query,esdelete_by_query_rethrottle,esexists,
+	esexists_source,esexplain,esfield_caps,esget,esgetscript,esgetsource,esindexsetting,esmget,esmtermvectors,esping,
+	esputscript,esrankeval,esreindex,esreindex_rethrottle,esrender_search_template,esscripts_painless_execute,
+	essearch_shards,estrans,esupdate,esupdate_by_query,esupdate_by_query_rethrottle
 	
 	include("transformation.jl")
 	include("api.jl")
