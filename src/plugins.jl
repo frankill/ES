6d7bf5f671a381de2b0883d3b156f561e5139f64
frankill <34159384+frankill@users.noreply.github.com>
2018-12-03 12:15:02 +0800

    Update plugins.jl

--- src/plugins.jl
+++ src/plugins.jl
@@ -10,26 +10,5 @@ function makeurl(::Type{Xpack{:translate}}, info::Esinfo)
 	"http://$(info.host):$(info.port)/_xpack/sql/translate"
 end
 
-# function Base.merge(a::AbstractString, b::AbstractString)
-# 	endswith(a, b) && return a 
-# 	join([a,b])
-# end  
-
-# macro xpackfun(interface) 
-# 	funname = esc(Symbol(merge("xpack", interface )) )
-# 	return quote 
-# 		iname = Symbol($interface)
-# 		function $(funname)(info::Esinfo, essql::T ; kw...) where T <: Union{Dict, AbstractString}
-# 			querys = Dict(kw...) 
-# 			isa(essql, Dict) && (essql = json(essql)) 
-# 			url   = makeurl(Xpack(iname), info) 
-# 			@esexport "POST" url essql querys "application/json"
-# 		end 
-# 	end
-# end 
-
-# @xpackfun "translate" 
-# @xpackfun "sql" 
-
 @genfunction "POST" xpacksql Xpack{:sql} 1
 @genfunction "POST" xpacktranslate Xpack{:translate} 1
