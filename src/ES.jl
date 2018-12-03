6dca6735ae2692ee92827636cacd52499f49eeda
frankill <34159384+frankill@users.noreply.github.com>
2018-12-03 11:30:38 +0800

    Update ES.jl

--- src/ES.jl
+++ src/ES.jl
@@ -60,7 +60,65 @@ module ES
 
 			end )
 	end
+	macro catexport(method, url , query  )
 
+		esc(
+			quote
+
+				respos = HTTP.request($method, $url, query= $query)
+				String(respos.body) |> println
+
+			end )
+	end
+
+	function genfunction(  kw::Vector  ) 
+
+		func = Expr(:call , 
+			kw[2] ,
+			Expr(:parameters, 
+				Expr(:(...), :kw)),
+			Expr(:(::) , :info , :Esinfo)
+			)
+
+		url = :( url = makeurl( $(kw[3]), info ) )
+
+		if length(kw) >= 5
+			append!( url.args[2].args , string.( kw[5:end] ) )
+			append!(func.args, Expr.(:(::) , kw[5:end] , :AbstractString))	
+		end 
+
+		if kw[4] >= 1    
+			push!(func.args,  Expr(:(::) , :body , :T) )
+			body = :body 
+		else 
+			body = Dict()
+		end 
+
+		block = Expr(:block ,  
+			:( querys = Dict(kw...) ),
+			url ,
+			Expr(:macrocall , 
+					 Symbol("@esexport") ,
+					 "",
+					 kw[1],
+					 :url ,
+					 Expr(:call , :ifelse , Expr(:call, :isa, body, :Dict), 
+								Expr(:call, :json, body) , body ) ,
+					 :querys ,
+					 "application/json"
+					 )
+				)
+
+		esc(Expr(:function, Expr(:where , func,  
+					Expr(:(<:) , :T , Expr(:curly,:Union, :Dict, :AbstractString))), block))
+
+	end 
+
+	macro genfunction( kw... )
+
+		 genfunction( collect(kw) ) 
+
+	end 
 	include("transformation.jl")
 	include("api.jl")
 	include("cat.jl")
