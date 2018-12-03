9ac42e4c751d0f97d5201d8391190fad324d13cb
frankill <34159384+frankill@users.noreply.github.com>
2018-12-03 11:40:27 +0800

    Create macro.jl

---
+++ src/macro.jl
@@ -0,0 +1,89 @@
+macro eshead( url , query  )
+
+	esc(
+		quote
+			try 
+				respos = HTTP.request("HEAD", $url , query= $query)
+
+				if respos.status == 200 
+					"OK"
+				end
+			catch 
+				"404 - Not Found"
+			end 
+
+		end )
+end
+
+macro esdelete( url,   query   )
+
+	esc(
+		quote
+
+			respos = HTTP.request("DELETE", $url ,  query= $query)
+
+			if respos.status == 200 
+				JSON.parse(String(respos.body))
+			end
+
+		end )
+end
+macro catexport(method, url , query  )
+
+	esc(
+		quote
+
+			respos = HTTP.request($method, $url, query= $query)
+			String(respos.body) |> println
+
+		end )
+end
+
+function genfunction(  kw::Vector  ) 
+
+	func = Expr(:call , 
+		kw[2] ,
+		Expr(:parameters, 
+			Expr(:(...), :kw)),
+		Expr(:(::) , :info , :Esinfo)
+		)
+
+	url = :( url = makeurl( $(kw[3]), info ) )
+
+	if length(kw) >= 5
+		append!( url.args[2].args , string.( kw[5:end] ) )
+		append!(func.args, Expr.(:(::) , kw[5:end] , :AbstractString))	
+	end 
+
+	if kw[4] >= 1    
+		push!(func.args,  Expr(:(::) , :body , :T) )
+		body = :body 
+	else 
+		body = Dict()
+	end 
+
+	block = Expr(:block ,  
+		:( querys = Dict(kw...) ),
+		url ,
+		Expr(:macrocall , 
+				 Symbol("@esexport") ,
+				 "",
+				 kw[1],
+				 :url ,
+				 Expr(:call , :ifelse , Expr(:call, :isa, body, :Dict), 
+							Expr(:call, :json, body) , body ) ,
+				 :querys ,
+				 "application/json"
+				 )
+			)
+
+	esc(Expr(:function, Expr(:where , func,  
+				Expr(:(<:) , :T , Expr(:curly,:Union, :Dict, :AbstractString))), block))
+
+end 
+
+macro genfunction( kw... )
+
+	 genfunction( collect(kw) ) 
+
+end 
