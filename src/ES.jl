b27f9b31390204cdf9e011d84432bffc2d9c14d7
frankill <34159384+frankill@users.noreply.github.com>
2018-12-19 15:26:13 +0800

    Update ES.jl

--- src/ES.jl
+++ src/ES.jl
@@ -25,7 +25,7 @@ module ES
 		es_indices_recovery,es_indices_refresh,es_indices_rollover,es_indices_segments,es_indices_shard_stores
 
 	export @esexport ,@query, @filter, @must, @must_not ,@should  ,@nested ,@has_child, @has_parent ,@fulltext,@smi,@comm,@extra
-	export BulkLength ,unique, findfirst
+	export BulkLength 
 
 	include("transformation.jl")
 	include("api.jl")
