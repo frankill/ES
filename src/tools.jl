1d3784df7528e44425a56b2b79c677d6b614158b
frankill <34159384+frankill@users.noreply.github.com>
2018-12-19 15:25:59 +0800

    Update tools.jl

--- src/tools.jl
+++ src/tools.jl
@@ -1,14 +1,1 @@
-function Base.findfirst(A::Vector{T}, name::AbstractString) where T <: Union{Dict, NamedTuple}
-    seen = Set{String }()
-    num = length(A)
-    res = Vector{Int}()
-    @inbounds for x in 1:num 
-        if A[x][name] ∉ seen
-            push!(seen, A[x][name])
-            push!(res, x)
-        end
-    end
-    res
-end
 
-Base.unique(A::Vector{T}, name::AbstractString) where T <: Union{Dict, NamedTuple} = findfirst(A, name) |> q -> view(A, q) 
