27c78a689f29d7689e544b5badec15be277a5f65
mac <mac@macs-iMac.local>
2019-01-09 10:48:08 +0800

    1

--- src/ES.jl
+++ src/ES.jl
@@ -46,8 +46,7 @@ module ES
 
 	end
 
-	Esinfo(host::AbstractString) = Esinfo(host, "9200")
-	Esinfo() = Esinfo("127.0.0.1")
+	Esinfo(host::AbstractString="127.0.0.1") = Esinfo(host, "9200")
 
 	include("macro.jl")
 	include("transformation.jl")
